bfb956443671af0bfbf36fb963080f0e1c1b2319
Erin-Boehmer <erin.boehmer@gmail.com>
2015-04-23 18:13:55 -0400

    Updated requirements - migrating to Softlayer

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,12 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template, request, jsonify
+import solr
 
 # create the application object
 app = Flask(__name__)
+
+# create a connection to the solr server
+s = solr.SolrConnection('http://localhost:8983/solr')
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -12,7 +16,8 @@ def home():
 
 @app.route('/query', methods=['POST'])
 def query():
-    query = ""
+    query = 'num_tree_i:10'
+    #response = s.query(query)
     print request.json["accuracies"]
     print request.json["tags"]
     return(jsonify(request.json))
