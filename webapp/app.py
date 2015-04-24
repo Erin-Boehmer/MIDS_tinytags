11d29b17f30d2e44c912cd3fcf4335a52aa5e111
Erin-Boehmer <erin.boehmer@gmail.com>
2015-04-23 21:45:40 -0500

    Returning true query results in json format

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,15 @@
-# import the Flask class from the flask module
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 import solr
+import random
+import StringIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 # create the application object
 app = Flask(__name__)
 
 # create a connection to the solr server
-s = solr.SolrConnection('http://localhost:8983/solr')
+s = solr.SolrConnection('http://localhost:8983/solr/TinyImages_shard2_replica1')
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -16,12 +19,56 @@ def home():
 
 @app.route('/query', methods=['POST'])
 def query():
-    query = 'num_tree_i:10'
-    #response = s.query(query)
-    print request.json["accuracies"]
-    print request.json["tags"]
-    return(jsonify(request.json))
+    # create the query string for tags
+    tag_lookup = {
+        "airplane":"0",
+        "automobile":"1",
+        "bird":"2",
+        "cat":"3",
+        "deer":"4",
+        "dog":"5",
+        "frog":"6",
+        "horse":"7",
+        "ship":"8",
+        "truck":"9" }
+    tag_str = ""
+    for tag in request.json['tags']:
+        if(tag_str == ""):
+            tag_str = tag_lookup[str(tag)]
+        else:
+            tag_str = "%s OR %s" % (tag_str, tag_lookup[str(tag)])
+    
+    # create the query string for accuracy
+    low_acc = int(request.json['accuracies'][0])/10
+    high_acc = int(request.json['accuracies'][1])/10
+    accuracy_str = 'num_tree_i:[%d TO %d]' % (low_acc, high_acc)
+    
+    # create the filter query and submit to solr
+    fq_str = "label_i:(%s) AND %s" % (tag_str, accuracy_str)
+    response = s.query('*:*', fq=fq_str)
+    
+    # create the image json response 
+    data = {"images": []}
+    for hit in response.results:
+        image = {"data":hit['data'], "label_i":hit['label_i'], "num_tree_i":hit['num_tree_i']}
+        data["images"].append(image)
+    return(jsonify(data))
 
-# start the server with the 'run()' method
+@app.route('/plot.png')
+def plot():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+ 
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+ 
+    axis.plot(xs, ys)
+    canvas = FigureCanvas(fig)
+    output = StringIO.StringIO()
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
